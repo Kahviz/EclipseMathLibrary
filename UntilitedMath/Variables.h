6f9906276f606322720033ebf2f2e250fbd74142
Kahviz <joakimukkonen@gmail.com>
2026-04-06 13:09:38 +0300

    Made variables names more obius

--- UntilitedMath/Variables.h
+++ UntilitedMath/Variables.h
@@ -4,12 +4,12 @@
 constexpr float PI = 3.14159265f; // I <3 Google
 constexpr float DOUBLE_PI = 2.0f * 3.14159265f;
 constexpr float HALF_PI = 3.14159265f / 2.0f;
-constexpr float Quarter_PI = 3.14159265f / 4.0f;
+constexpr float QUARTER_PI = 3.14159265f / 4.0f;
 constexpr float CIRCLE_PI = PI * 2.0f;
 
 constexpr float FLOAT_HUGE = 3.402823466e+38f;
 
-constexpr float Gravity = 9.80665f; // I <3 Google
+constexpr float GRAVITY = 9.80665f; // I <3 Google
 constexpr float MOON_GRAVITY = 1.625f; // I <3 Google
 
 //Ints
