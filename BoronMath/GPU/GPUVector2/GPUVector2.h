feff09c4f4b0a30384329d8ebe6f59997a20e2d6
Kahviz <joakimukkonen@gmail.com>
2026-09-03 15:04:48 +0300

    Fixed sp GPUVector2 actually only countains 8 bytes of data float x, float y

--- BoronMath/GPU/GPUVector2/GPUVector2.h
+++ BoronMath/GPU/GPUVector2/GPUVector2.h
@@ -5,57 +5,53 @@ struct GPUVector2
 {
 	float x;
 	float y;
-	float z;
 
 	inline GPUVector2& operator+=(const GPUVector2& other) {
 		x += other.x;
 		y += other.y;
-		z += other.z;
 		return *this;
 	}
 
 	inline GPUVector2& operator-=(const GPUVector2& other) {
 		x -= other.x;
 		y -= other.y;
-		z -= other.z;
 		return *this;
 	}
 };
 
 inline std::ostream& operator<<(std::ostream& os, const GPUVector2& v)
 {
-	return os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+	return os << "(" << v.x << ", " << v.y << ")";
 }
 
 inline GPUVector2 operator+(const GPUVector2& a, const GPUVector2& b)
 {
-	return GPUVector2{ a.x + b.x, a.y + b.y, a.z + b.z };
+	return GPUVector2{ a.x + b.x, a.y + b.y };
 }
 
 inline GPUVector2 operator-(const GPUVector2& a, const GPUVector2& b)
 {
-	return GPUVector2{ a.x - b.x, a.y - b.y, a.z - b.z };
+	return GPUVector2{ a.x - b.x, a.y - b.y };
 }
 
 inline GPUVector2 operator*(const GPUVector2& a, float scalar)
 {
-	return GPUVector2{ a.x * scalar, a.y * scalar, a.z * scalar };
+	return GPUVector2{ a.x * scalar, a.y * scalar };
 }
 
 inline GPUVector2 operator/(const GPUVector2& a, float scalar)
 {
-	return GPUVector2{ a.x / scalar, a.y / scalar, a.z / scalar };
+	return GPUVector2{ a.x / scalar, a.y / scalar };
 }
 
 inline float dot(const GPUVector2& a, const GPUVector2& b)
 {
-	return a.x * b.x + a.y * b.y + a.z * b.z;
+	return a.x * b.x + a.y * b.y;
 }
 
 inline float MagnitudeGPUVector2(GPUVector2 a, GPUVector2 b) {
 	float dx = abs(a.x - b.x);
-	float dz = abs(a.z - b.z);
 	float dy = abs(a.y - b.y);
 
-	return dx + dz + dy;
+	return dx + dy;
 }
